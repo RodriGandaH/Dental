c1288773160c07ab2ba74a8bd9ef854ce262a2c9
Rodrigo Gandarillas Heredia <rodrigp.gandarillas.heredia@gmail.com>
2023-01-22 13:40:03 -0400

    iniciar sesion con nombre de usuario y recuperar contraseña con correo

--- database/migrations/2023_01_15_185312_create_patients_table.php
+++ database/migrations/2023_01_15_185312_create_patients_table.php
@@ -15,14 +15,13 @@ return new class extends Migration
  {
   Schema::create('patients', function (Blueprint $table) {
    $table->id();
-   $table->integer('user_id');
+   $table->unsignedBigInteger('user_id')->nullable()->foreign()->references('id')->on('users');
    $table->string('name');
    $table->date('birthdate');
    $table->integer('ci')->unique();
    $table->integer('phone')->unique();
    $table->string('address');
    $table->timestamps();
-   $table->foreign('user_id')->references('id')->on('users');
 
   });
  }
