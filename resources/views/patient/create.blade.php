a31ddc3e19d6ec54c3e1817145adb425696e9831
Rodrigo Gandarillas Heredia <rodrigp.gandarillas.heredia@gmail.com>
2023-02-08 17:49:25 -0400

    cambiando botones guardar

--- resources/views/patient/create.blade.php
+++ resources/views/patient/create.blade.php
@@ -364,7 +364,7 @@
                         placeholder="Escribe los message aqui..."></textarea>
                     <div class="flex justify-between">
                         <x-primary-button class="mt-3 ml-3">
-                            {{ __('Guardar Paciente') }}
+                            {{ __('Guardar') }}
                         </x-primary-button>
                         <a class='inline-flex items-center px-4 mt-3 py-2 bg-red-600 border
         border-gray-300 rounded-md font-semibold text-xs text-white uppercase tracking-widest shadow-sm hover:bg-red-400
